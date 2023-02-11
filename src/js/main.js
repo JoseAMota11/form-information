db424435ebf913b4c7a6ec40fddda9ad9f3bf49e
José Mota <jose.a.mota11@gmail.com>
2023-02-10 20:56:31 -0600

    :sparkles: Adding Konami Code

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import { formEventListener } from "./formFunction.js";
+import { konami } from "./konami.js";
 
 const form = document.getElementById('formSummit');
 
 form.addEventListener('submit', formEventListener);
+
+window.addEventListener('keyup', konami)
